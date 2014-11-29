e2e003e048e75f548c6747eee87fd68cf0e332df
khocef <moncef.khoulfi@gmail.com>
2014-11-30 00:19:02 +0100

    add implementation of configuration and component

--- fr.univnantes.alma.hadl.m2/src/fr/univnantes/alma/hadl/m2/Component.java
+++ fr.univnantes.alma.hadl.m2/src/fr/univnantes/alma/hadl/m2/Component.java
@@ -1,9 +1,41 @@
 package fr.univnantes.alma.hadl.m2;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public abstract class Component extends ArchitecturalElement {
+
+	private Set<Interface> interfaces;
+	private Set<Link> links;
 
 	public Component(String name) {
 		super(name);
+		this.interfaces = new HashSet<Interface>();
+		this.links = new HashSet<Link>();
 	}
 
+	public boolean containsInterface(Interface _interface) {
+		return this.interfaces.contains(_interface);
+	}
+
+	public boolean addInterface(Interface _interface) {
+		return this.interfaces.add(_interface);
+	}
+
+	public boolean addInterfaces(Set<Interface> interfaces) {
+		return this.interfaces.addAll(interfaces);
+	}
+
+	public boolean addInterfaces(Interface... interfaces) {
+		boolean b = true;
+		for (Interface i : interfaces)
+			this.interfaces.add(i);
+		return b;
+	}
+
+	public boolean removeInterface(Interface _interface) {
+		return this.interfaces.remove(_interface);
+	}
+
+	// TODO : add methods for Links
 }
--- fr.univnantes.alma.hadl.m2/src/fr/univnantes/alma/hadl/m2/Configuration.java
+++ fr.univnantes.alma.hadl.m2/src/fr/univnantes/alma/hadl/m2/Configuration.java
@@ -2,6 +2,11 @@ package fr.univnantes.alma.hadl.m2;
 
 import java.util.Set;
 
+/**
+ * 
+ * @author khocef
+ *
+ */
 public abstract class Configuration extends Component {
 
 	private Set<ArchitecturalElement> elements;
@@ -19,14 +24,36 @@ public abstract class Configuration extends Component {
 	}
 
 	/**
-	 * @param elements the elements to set
+	 * @param elements
+	 *            the elements to set
 	 */
 	private void setElements(Set<ArchitecturalElement> elements) {
 		this.elements = elements;
 	}
 
-	public void addComponent(ArchitecturalElement component) {
-		
+	public boolean containsElement(ArchitecturalElement element) {
+		return this.elements.contains(element);
 	}
 
+	public boolean addElement(ArchitecturalElement element) {
+		element.setConfiguration(this);
+		return this.elements.add(element);
+	}
+
+	public boolean addElements(Set<ArchitecturalElement> elements) {
+		for (ArchitecturalElement e : elements)
+			e.setConfiguration(this);
+		return this.elements.addAll(elements);
+	}
+
+	public boolean addElements(ArchitecturalElement... elements) {
+		boolean b = true;
+		for (ArchitecturalElement e : elements)
+			b &= this.elements.add(e);
+		return b;
+	}
+
+	public boolean removeElement(ArchitecturalElement element) {
+		return this.elements.remove(element);
+	}
 }
